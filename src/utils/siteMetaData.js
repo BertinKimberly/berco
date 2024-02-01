2fbdd40964bd1a27711376ed2bba7bed99eec13f
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2024-02-01 19:24:50 +0200

    initial commit

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+   title: "Berco , Learn more about  technology and programming",
+   author: "Bertin",
+   headerTitle: "Berco , a programming blog",
+   description: "A blog with programming information",
+   language: "en-us",
+   theme: "system", // system, dark or light
+   siteUrl: "https://create-blog-with-nextjs.vercel.app", // your website URL
+   siteLogo: "/logo.png",
+   socialBanner: "/social-banner.png", // add social banner in the public folder
+   email: "iradukundabertin082@gmail.com",
+   github: "https://github.com/BertinKimberly",
+   twitter: "https://twitter.com/code_bucks",
+   facebook: "https://facebook.com/",
+   youtube: "https://youtube.com/codebucks",
+   linkedin: "https://www.linkedin.com/in/iradukunda-bertin-074337261/",
+   dribbble: "https://www.dribbble.com",
+   locale: "en-US",
+};
+
+module.exports = siteMetadata;
