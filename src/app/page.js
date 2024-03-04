34b04bf85817e6348305c58322979f1aa61a5f6d
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2024-03-04 21:12:54 +0200

    update a post

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,9 @@ import RecentPosts from "../components/Home/RecentPosts";
 export default function Home() {
    return (
       <main className='flex flex-col items-center justify-center'>
-         <h1 className='text-4xl mt-10 text-center mx-10 text-blue-500 dark:text-white'>
-            Welcome to Berco . Learn best software approaches to solve different problems.Have fun at Berco.
+         <h1 className='text-5xl mt-10 text-center mx-10 text-blue-500 dark:text-white leading-normal'>
+            Welcome to Berco . Learn best software approaches to solve different
+            problems.Have fun at Berco.
          </h1>
          <HomeCoverSection blogs={allBlogs} />
          <FeaturedPosts blogs={allBlogs} />
