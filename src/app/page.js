871724927a295ddab6e2c58d4da9c758b0595372
bertin <iradukundabertin082@gmail.com>
2024-04-14 15:39:21 +0200

    modify scroll to top

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,6 @@ import RecentPosts from "../components/Home/RecentPosts";
 export default function Home() {
    return (
       <main className='flex flex-col items-center justify-center'>
-         <h1 className='text-5xl mt-10 text-center mx-10 text-blue-500 dark:text-white leading-normal'>
-            Welcome to Berco . Learn best software approaches to solve different
-            problems.Have fun at Berco.
-         </h1>
          <HomeCoverSection blogs={allBlogs} />
          <FeaturedPosts blogs={allBlogs} />
          <RecentPosts blogs={allBlogs} />
--- src/components/Elements/Upward.js
+++ src/components/Elements/Upward.js
@@ -1,9 +1,31 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Upward = () => {
+   const [isScrolling, setIsScrolling] = useState(false);
+   const scroll = () => {
+      window.scrollTo(0, 0);
+   };
+   useEffect(() => {
+      const handleScroll = () => {
+         if (window.scrollY > 50) {
+            setIsScrolling(true);
+         } else {
+            setIsScrolling(false);
+         }
+      };
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+         window.removeEventListener("scroll", handleScroll);
+      };
+   }, []);
    return (
-      <div className=' p-2 fixed bottom-14 right-4 rounded bg-dark dark:bg-light text-light dark:text-dark cursor-pointer '>
+      <div
+         className={`p-2 fixed bottom-14 right-4 rounded bg-dark dark:bg-light text-light dark:text-dark cursor-pointer  ${
+            isScrolling ? "" : "hidden"
+         }`}
+         onClick={scroll}
+      >
          <p>Scroll To Top</p>
       </div>
    );
